6ae6e1d31bb1c6f276ec03ef60e9e08e2bd08ccc
Kevin Villegas Pérez <villegasdeveloper@outlook.es>
2025-12-05 11:36:21 -0500

    first commit

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,9 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+describe('AppController', () => {
+  beforeEach(async () => {});
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {});
+  });
+});
